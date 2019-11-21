ba5efac89f3c9a238a580e3e61bd07b4c7613be1
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-11-21 14:22:21 +0200

    Configured Eslint and ran autofixes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const App = ({ booklist }) => {
-  const [books, setBooks] = useState(booklist)
-  const [name, setName] = useState('')
-  const [author, setAuthor] = useState('')
-  const addBook = event => {
-    event.preventDefault()
-    setBooks(books.concat(
-      {
-        name: name,
-        author: author
-      }
-    ))
-  }
+    const [books, setBooks] = useState(booklist)
+    const [name, setName] = useState('')
+    const [author, setAuthor] = useState('')
+    const addBook = (event) => {
+        event.preventDefault()
+        setBooks(books.concat(
+            {
+                name,
+                author,
+            },
+        ))
+    }
 
-  return (
-    <div>
-      <form onSubmit={addBook}>
-        <input type='text' value={name} onChange={event => setName(event.target.value)}/>
-        <input type='text' value={author} onChange={event => setAuthor(event.target.value)}/>
-        <button type="submit">add book</button> 
-      </form>
+    return (
+        <div>
+            <form onSubmit={addBook}>
+                <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+                <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} />
+                <button type="submit">add book</button>
+            </form>
 
-      <ul>
-        { books.map(book => <li key={book.id}>{book.name}</li>) }
-      </ul>
-    </div>
-  )
+            <ul>
+                { books.map((book) => <li key={book.id}>{book.name}</li>) }
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 const booklist = [
-  {
-    id: 1,
-    name: "Taru sormusten herrasta",
-    author: "J.R.R Tolkien"
-  },
-  {
-    id: 2,
-    name: "Sieppari ruispellossa",
-    author: "J.D Salinger"
-  }
+    {
+        id: 1,
+        name: 'Taru sormusten herrasta',
+        author: 'J.R.R Tolkien',
+    },
+    {
+        id: 2,
+        name: 'Sieppari ruispellossa',
+        author: 'J.D Salinger',
+    },
 ]
 
-ReactDOM.render(<App booklist={booklist} />, document.getElementById('root'));
-
+ReactDOM.render(<App booklist={booklist} />, document.getElementById('root'))
