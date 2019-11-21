bd6e79a4e88c6623181c57032c9f51a5181b6e06
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-11-21 14:02:08 +0200

    can add books

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const App = ({ booklist }) => {
   const [books, setBooks] = useState(booklist)
+  const [name, setName] = useState('')
+  const [author, setAuthor] = useState('')
+  const addBook = event => {
+    event.preventDefault()
+    setBooks(books.concat(
+      {
+        name: name,
+        author: author
+      }
+    ))
+  }
+
   return (
     <div>
+      <form onSubmit={addBook}>
+        <input type='text' value={name} onChange={event => setName(event.target.value)}/>
+        <input type='text' value={author} onChange={event => setAuthor(event.target.value)}/>
+        <button type="submit">add book</button> 
+      </form>
+
       <ul>
-        { books.map(book => <li>{book.name}</li>) }
+        { books.map(book => <li key={book.id}>{book.name}</li>) }
       </ul>
     </div>
   )
