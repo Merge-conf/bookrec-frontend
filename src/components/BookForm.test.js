efeece7224cf51cc42d1b41ea34bcc2546e2f30b
HegePI <19744083+HegePI@users.noreply.github.com>
2019-11-22 18:17:25 +0200

    bookFrom.test modified

--- src/components/BookForm.test.js
+++ src/components/BookForm.test.js
@@ -4,15 +4,12 @@ import { render, fireEvent } from '@testing-library/react'
 import BookForm from './BookForm'
 
 const BookFormWrapper = (props) => {
-  const setName = (event) => {
-    console.log(`nimi: ${event.target.value}`)
-    props.state.name = event.target.value.name
+  const setName = (value) => {
+    props.state.name = value
   }
-  const setAuthor = (event) => {
-    console.log(`kirjoittaja: ${event.target.value}`)
-    props.state.author = event.target.value
+  const setAuthor = (value) => {
+    props.state.author = value
   }
-
 
   return (
     <BookForm addBook={props.onSubmit} setName={setName} setAuthor={setAuthor} />
@@ -30,7 +27,7 @@ test('Can add book', () => {
   const input = component.container.querySelector('input')
   const form = component.container.querySelector('form')
 
-  fireEvent.change(input, { target: { Name: 'testing', Author: 'MR.Merge' } })
+  fireEvent.change(input, { target: { value: 'testing' } })
   fireEvent.submit(form)
 
   expect(onSubmit.mock.calls.length).toBe(1)
