5d58a6cb04916afa54f595cca8b4cf8a239c72bb
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-11-24 18:11:18 +0200

    Changed books in the cypress test to match the ones on the backend testserver

--- cypress/integration/view_books.js
+++ cypress/integration/view_books.js
@@ -4,8 +4,8 @@ describe('Books can be viewed', function () {
   })
 
   it('Website should contain a example book', function () {
-    cy.get('ul').children().contains('Kirjoittaja: J.K. Raimo')
-    cy.get('ul').children().contains('otsikko: Harri Puotila ja Kallion Alepa')
+    cy.get('ul').children().contains('Kirjoittaja: Who Cares')
+    cy.get('ul').children().contains('otsikko: 51 shades of gray')
   })
 
 })
