b7dc2422ff485a5b079342971a410301c5180091
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-11-24 18:48:11 +0200

    Configured test to clear test server and add a certain book before test

--- cypress/integration/view_books.js
+++ cypress/integration/view_books.js
@@ -1,11 +1,23 @@
 describe('Books can be viewed', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const book = {
+      name: 'Homo Deus',
+      author: 'Yuval Noah Harari'
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/books/', book)
+
+  })
+
   it('Website can be accessed', function () {
     cy.visit('/')
   })
 
   it('Website should contain a example book', function () {
-    cy.get('ul').children().contains('Kirjoittaja: Who Cares')
-    cy.get('ul').children().contains('otsikko: 51 shades of gray')
+    cy.get('ul').children().contains('Kirjoittaja: Yuval Noah Harari')
+    cy.get('ul').children().contains('otsikko: Homo Deus')
   })
 
 })
