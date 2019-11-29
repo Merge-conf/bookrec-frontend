97c0b297233e1ac6e0a1489a506589ac4af98bc0
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-11-29 11:11:16 +0200

    make list look nicer

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,44 @@
 import React from 'react'
 
 const List = ({ items, filter }) => {
-  const filterItems = () => items.filter((item) => item.name.toUpperCase().includes(filter.toUpperCase()))
+  const filterItems = (list) => list.filter((item) => {
+    const itemUpper = item.name.toUpperCase()
+    const filterUpper = filter.toUpperCase()
+    return itemUpper.includes(filterUpper)
+  })
 
-  const mapItems = () => filterItems().map((item) => <li>{item.name}</li>)
+  const mapItems = (list) => list.map((item) => {
+    if (!item.name) return <li id={item.id}>no name</li>
+    if (item.author) return <Book id={item.id} name={item.name} author={item.author} />
+    if (item.creator && item.url) return <Audio id={item.id} name={item.name} creator={item.creator} url={item.url} />
+    return <li>not book or audio</li>
+  })
 
   return (
     <div>
       <h2>Recommendation list</h2>
       <ul>
-        {mapItems()}
+        {mapItems(filterItems(items))}
       </ul>
     </div>
+  )
+}
+
+
+const Book = ({ id, name, author }) => {
+  return (
+    <li id={id}>
+      {`Nimi: ${name}, `}
+      {`Kirjoittaja: ${author}`}
+    </li>
+  )
+}
+
+const Audio = ({ id, name, creator, url }) => {
+  return (
+    <li id={id}>
+      {`Otsikko: ${name} - ${creator}`}
+    </li>
   )
 }
 
