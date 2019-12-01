ced879d967ed28b4998a74b26679deb54fc109b0
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-12-01 17:55:39 +0200

    Modified list to show audio files without creator

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ const List = ({ items, filter, checkFilter }) => {
     const itemUpper = item.name.toUpperCase()
     const filterUpper = filter.toUpperCase()
     const isBook = Boolean(item.author)
-    const isAudio = Boolean(item.creator && item.url)
+    const isAudio = Boolean(item.url)
     if (isBook && !checkFilter.books) return false
     if (isAudio && !checkFilter.audio) return false
     return itemUpper.includes(filterUpper)
@@ -15,7 +15,7 @@ const List = ({ items, filter, checkFilter }) => {
   const mapItems = (list) => list.map((item) => {
     if (!item.name) return <li key={item.id}>no name</li>
     if (item.author) return <BookRec key={item.id} name={item.name} author={item.author} />
-    if (item.creator && item.url) return <AudioRec key={item.id} name={item.name} creator={item.creator} url={item.url} />
+    if (item.url) return <AudioRec key={item.id} name={item.name} creator={item.creator} url={item.url} />
     return <li>not book or audio</li>
   })
 
