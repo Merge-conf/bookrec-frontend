adcb251f116dccc90ad5fc8817e8b06edf59fa14
universami <sami.toiskallio@protonmail.com>
2019-12-06 20:39:30 +0200

    add functionality to play audio on single click

--- src/components/Tables/AudioTable.js
+++ src/components/Tables/AudioTable.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import DataTable from 'react-data-table-component'
 import Popup from 'reactjs-popup'
 import audioService from '../../services/audioService'
+import Player from '../Player'
 
 const AudioTable = ({ setData, data, render }) => {
   const [inEdit, setInEdit] = useState(null)
   const [name, setName] = useState('')
   const [creator, setCreator] = useState('')
+  const [url, setUrl] = useState('')
   const columns = [
     {
       name: 'creator',
@@ -54,7 +56,9 @@ const AudioTable = ({ setData, data, render }) => {
           <button type="submit">edit</button>
         </form>
       </Popup>
-      <DataTable onRowClicked={(item) => setInEdit(item)} selectableRows title="Audio" columns={columns} data={data} />
+      <DataTable onRowDoubleClicked={(item) => setInEdit(item)} selectableRows title="Audio (click to play)" columns={columns} data={data}
+        onRowClicked={(item) => setUrl(item.url)} />
+      <Player url={url} />
     </div>
   )
 }
