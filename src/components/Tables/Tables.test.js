073d650f2132236ff3bdd064152d60ccf1fdf495
Gert Adamson <adamson.gert@hotmail.com>
2019-12-06 17:43:01 +0200

    create unit tests for viewing and sorting tables

--- src/components/Tables/Tables.test.js
+++ src/components/Tables/Tables.test.js
@@ -4,32 +4,31 @@ import { render, cleanup } from '@testing-library/react'
 import Tables from './Tables'
 
 let container
-let getByText
 
 const books = [
   {
     name: 'Lord of the Rings',
     author: 'Tolkien',
-    id: '1',
+    id: 'book1',
   },
   {
     name: 'Harry Potter',
     author: 'Rowling',
-    id: '2',
+    id: 'book2',
   },
 ]
 const audio = [
   {
     name: 'Podcast',
-    creator: 'Anon',
+    creator: 'Creed',
     url: 'www.osoite.fi',
-    id: '1',
+    id: 'audio1',
   },
   {
     name: 'Nature sounds',
     creator: 'BBC',
     url: 'www.bbs.com',
-    id: '2',
+    id: 'audio2',
   },
 ]
 const setBooks = jest.fn()
@@ -37,7 +36,7 @@ const setAudio = jest.fn()
 
 describe('<Tables />', () => {
   beforeEach(() => {
-    ({ container, getByText } = render(
+    ({ container } = render(
       <Tables books={books} setBooks={setBooks} audio={audio} setAudio={setAudio} />
     ))
   })
@@ -58,8 +57,44 @@ describe('<Tables />', () => {
   })
 
   test('audio creators are shown by default', () => {
-    expect(container).toHaveTextContent('Lord of the Rings')
-    expect(container).toHaveTextContent('Harry Potter')
+    expect(container).toHaveTextContent('Creed')
+    expect(container).toHaveTextContent('BBC')
+  })
+
+  test('books can be sorted by name', () => {
+    let firstBook = container.querySelector('[id^=row-book]')
+    expect(firstBook).toHaveTextContent('Lord of the Rings')
+
+    container.querySelector('[id=column-name]').click()
+    firstBook = container.querySelector('[id^=row-book]')
+    expect(firstBook).toHaveTextContent('Harry Potter')
+  })
+
+  test('books can be sorted by author', () => {
+    let firstBook = container.querySelector('[id^=row-book]')
+    expect(firstBook).toHaveTextContent('Tolkien')
+
+    container.querySelector('[id=column-author]').click()
+    firstBook = container.querySelector('[id^=row-book]')
+    expect(firstBook).toHaveTextContent('Rowling')
+  })
+
+  test('audio can be sorted by name', () => {
+    let firstAudio = container.querySelector('[id^=row-audio]')
+    expect(firstAudio).toHaveTextContent('Podcast')
+
+    container.querySelectorAll('[id=column-name]')[1].click()
+    firstAudio = container.querySelector('[id^=row-audio]')
+    expect(firstAudio).toHaveTextContent('Nature sounds')
+  })
+
+  test('audio can be sorted by creator', () => {
+    let firstAudio = container.querySelector('[id^=row-audio]')
+    expect(firstAudio).toHaveTextContent('Creed')
+
+    container.querySelector('[id=column-creator]').click()
+    firstAudio = container.querySelector('[id^=row-audio]')
+    expect(firstAudio).toHaveTextContent('BBC')
   })
 
   afterEach(cleanup)
